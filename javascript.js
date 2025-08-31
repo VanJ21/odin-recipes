dc28d40719bd314ed0571fc04241e10ad0c3bdda
VanJ21 <87626224+VanJ21@users.noreply.github.com>
2025-08-31 11:11:07 -0400

    Messing around with js

---
+++ javascript.js
@@ -0,0 +1,18 @@
+let name = "John";
+console.log(name);
+console.log(23 + 97);
+console.log(23 + 97 + 40 + 30 + 20 + 10);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10; 
+console.log(a);
+a = 15; 
+console.log(a);
+
+let b = 7 * a; 
+console.log(b);
+
+const max = 57; 
+const actual = max - 13; 
+const percentage = actual / max; 
+console.log(percentage);
